cb633aeb6241b9fb42d287490be738d0b8437adb
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-15 14:29:02 +0700

    fix height mobile

--- src/components/Navbar/Navbar.elements.js
+++ src/components/Navbar/Navbar.elements.js
@@ -68,7 +68,7 @@ export const NavMenu = styled.ul`
 
 	@media screen and (max-width: 960px) {
 		width: 100%;
-		height: 90vh;
+		height: calc(100vh - 80px);
 		background: #101522;
 
 		display: flex;
