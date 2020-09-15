f1183d33ec7141765828ed537fa940d680e9bd91
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-15 12:37:48 +0700

    import GlobalStyles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Navbar } from "./components";
+import GlobalStyle from "./GlobalStyles";
 
 import { NotFound, Spinner } from "./components/UI/";
 import ScrollToTop from "./components/ScrollToTop";
@@ -10,6 +11,7 @@ function App() {
 	return (
 		<Suspense fallback={<Spinner />}>
 			<Router>
+				<GlobalStyle />
 				<ScrollToTop />
 				<Navbar />
 				<Switch>
