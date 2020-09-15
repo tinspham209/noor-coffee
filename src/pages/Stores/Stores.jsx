817378b6a4e089d716a6db12ca265ef6adbe2e18
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-15 14:50:33 +0700

    stores pages

---
+++ src/pages/Stores/Stores.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { InfoSection } from "../../components";
+import { homeObjOne } from "./Data";
+const Stores = () => {
+	return (
+		<>
+			<InfoSection {...homeObjOne} />
+		</>
+	);
+};
+
+export default Stores;
