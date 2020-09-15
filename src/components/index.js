85099418b610b614eb2faf84168244c8d40c6fb4
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-15 14:24:36 +0700

    export component

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as InfoSectionFull } from "./InfoSectionFull/InfoSectionFull";
+export { default as Footer } from "./Footer/Footer";
