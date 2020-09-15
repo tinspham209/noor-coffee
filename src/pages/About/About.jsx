c7a3bcf2a4a53986c9224c669dc39117255c5c78
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-15 14:50:13 +0700

    About page

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { InfoSection } from "../../components";
+import { homeObjOne } from "./Data";
+const About = () => {
+	return (
+		<>
+			<InfoSection {...homeObjOne} />
+		</>
+	);
+};
+
+export default About;
