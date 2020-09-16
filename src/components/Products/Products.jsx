eecdea64f7937df6508d90ff9f98c53c6feb96de
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-16 14:19:50 +0700

    Deploy

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-// import { HashLink as Link } from "react-router-hash-link";
+// import { Link } from "react-router-dom";
+import { HashLink as Link } from "react-router-hash-link";
 import { Container } from "../../GlobalStyles";
 import {
 	Background,
