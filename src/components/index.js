2212a22a335a231d0b257362fd74a2eba345f48a
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-23 12:04:06 +0700

    export productList

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as InfoSection } from "./InfoSection/InfoSection";
 export { default as Footer } from "./Footer/Footer";
 export { default as StoreMaps } from "./StoreMaps/StoreMaps";
 export { default as ProductsComponent } from "./Products/Products";
+export { default as ProductsList } from "./Products/ProductList/ProductList";
