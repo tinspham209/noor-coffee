1d5e84afbd310383bc4203c9edc0af73900ddbac
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-23 12:02:42 +0700

    update globalStyles

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,12 +1,13 @@
 import styled, { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
- *{
-   box-sizing: border-box;
-   margin: 0;
-   padding: 0;
-   font-family: "Source Sans Pro", sans-serif;
- }
+	*{
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+		font-family: "Source Sans Pro", sans-serif;
+	}
+
   ::-webkit-scrollbar {
     width: 12px;
   }
