5f77b40da86c0cbac04782411c3293f5c6c80527
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-23 21:33:23 +0700

    remove custom scroll bar

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -8,28 +8,6 @@ const GlobalStyle = createGlobalStyle`
 		font-family: "Source Sans Pro", sans-serif;
 	}
 
-  ::-webkit-scrollbar {
-    width: 12px;
-  }
- 
-  /* Track */
-  ::-webkit-scrollbar-track {
-    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); 
-    -webkit-border-radius: 10px;
-    border-radius: 10px;
-  }
-  
-  /* Handle */
-  ::-webkit-scrollbar-thumb {
-    -webkit-border-radius: 10px;
-    border-radius: 10px;
-    background: rgba(0,0,0,0.8); 
-    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.5); 
-  }
-  ::-webkit-scrollbar-thumb:window-inactive {
-    background: rgba(0,0,0,0.4); 
-  }
-
 	a, a:hover, a:focus, a:active {
       text-decoration: none;
 	}
