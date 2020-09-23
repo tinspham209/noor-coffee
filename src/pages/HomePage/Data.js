988f6905d051ef49f0622e4e18d38f778969b883
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-23 12:03:20 +0700

    update ProductList component

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -15,58 +15,56 @@ export const special = {
 	lightText: true,
 	lightTextCard: false,
 	buttonLabel: "Chi tiết",
-	productList: [
-		{
-			id: "special",
-			headlineName: "menu",
-			headlineBtn: true,
-			headlineButtonName: "XEM THÊM TẤT CẢ SẢN PHẨM",
-			products: [
-				{
-					cardTitle: "Cà phê Quế",
-					img: require("../../assets/img/products/cf-que.jpg"),
-					description: "25.000Đ",
+	productList: {
+		id: "special",
+		headlineName: "menu",
+		headlineBtn: true,
+		headlineButtonName: "XEM THÊM TẤT CẢ SẢN PHẨM",
+		products: [
+			{
+				cardTitle: "Cà phê Quế",
+				img: require("../../assets/img/products/cf-que.jpg"),
+				description: "25.000Đ",
 
-					urlLinkBtn: "/products",
-					trend: true,
-					newProduct: true,
-				},
-				{
-					cardTitle: "trà atiso macchiato",
-					img: require("../../assets/img/products/macchiato-tra-atiso.jpg"),
-					description: "25.000Đ",
+				urlLinkBtn: "/products",
+				trend: true,
+				newProduct: true,
+			},
+			{
+				cardTitle: "trà atiso macchiato",
+				img: require("../../assets/img/products/macchiato-tra-atiso.jpg"),
+				description: "25.000Đ",
 
-					urlLinkBtn: "/products",
-				},
-				{
-					cardTitle: "Sữa chua việt quốc hạt chia ",
-					img: require("../../assets/img/products/yogurt-hat-chia-viet-quoc.jpg"),
-					description: "25.000Đ",
+				urlLinkBtn: "/products",
+			},
+			{
+				cardTitle: "Sữa chua việt quốc hạt chia ",
+				img: require("../../assets/img/products/yogurt-hat-chia-viet-quoc.jpg"),
+				description: "25.000Đ",
 
-					urlLinkBtn: "/products",
-				},
-				{
-					cardTitle: "trà hoa đậu biếc macchiato",
-					img: require("../../assets/img/products/macchiato-hoa-dau-biet.jpg"),
-					description: "25.000Đ",
+				urlLinkBtn: "/products",
+			},
+			{
+				cardTitle: "trà hoa đậu biếc macchiato",
+				img: require("../../assets/img/products/macchiato-hoa-dau-biet.jpg"),
+				description: "25.000Đ",
 
-					urlLinkBtn: "/products",
-				},
-				{
-					cardTitle: "nước ép củ dền",
-					img: require("../../assets/img/products/fruit-cu-den.jpg"),
-					description: "25.000Đ",
+				urlLinkBtn: "/products",
+			},
+			{
+				cardTitle: "nước ép củ dền",
+				img: require("../../assets/img/products/fruit-cu-den.jpg"),
+				description: "25.000Đ",
 
-					urlLinkBtn: "/products",
-				},
-				{
-					cardTitle: "cold brew cam vàng",
-					img: require("../../assets/img/products/coldbrew-cam-vang-outside.jpg"),
-					description: "25.000Đ",
+				urlLinkBtn: "/products",
+			},
+			{
+				cardTitle: "cold brew cam vàng",
+				img: require("../../assets/img/products/coldbrew-cam-vang-outside.jpg"),
+				description: "25.000Đ",
 
-					urlLinkBtn: "/products",
-				},
-			],
-		},
-	],
+				urlLinkBtn: "/products",
+			},
+		],
+	},
 };
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,6 @@
 import React, { useEffect } from "react";
-import {
-	InfoSectionFull,
-	StoreMaps,
-	ProductsComponent,
-} from "../../components";
+import { InfoSectionFull, StoreMaps, ProductsList } from "../../components";
+import { Container } from "../../GlobalStyles";
 import { hero, special } from "./Data";
 import { Maps } from "../Stores/Data";
 
@@ -11,11 +8,33 @@ const HomePage = () => {
 	useEffect(() => {
 		document.title = "Noor - Coffee & Tea";
 	}, []);
+
 	return (
 		<>
 			<InfoSectionFull {...hero} />
 			<StoreMaps {...Maps} />
-			<ProductsComponent products={special} />
+			{/* <ProductsComponent products={special} /> */}
+
+			<div
+				style={{
+					background: "#101522",
+					height: "auto",
+					padding: "80px 0",
+				}}
+			>
+				<Container>
+					<ProductsList
+						lightText={special.lightText}
+						lightTextCard={special.lightTextCard}
+						buttonLabel={special.buttonLabel}
+						id={special.productList.id}
+						headlineName={special.productList.headlineName}
+						headlineBtn={special.productList.headlineBtn}
+						headlineButtonName={special.productList.headlineButtonName}
+						productList={special.productList.products}
+					/>
+				</Container>
+			</div>
 		</>
 	);
 };
