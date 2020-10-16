5d1164d5f53ed0ac56659b8b0022864830bbee57
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-10-16 16:16:52 +0700

    getUUID

--- src/utils.js
+++ src/utils.js
@@ -7,3 +7,14 @@ export function phoneNumber(input) {
 		return false;
 	}
 }
+
+export function getUUID() {
+	// eslint gets funny about bitwise
+	/* eslint-disable */
+	return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+		const piece = (Math.random() * 16) | 0;
+		const elem = c === "x" ? piece : (piece & 0x3) | 0x8;
+		return elem.toString(16);
+	});
+	/* eslint-enable */
+}
